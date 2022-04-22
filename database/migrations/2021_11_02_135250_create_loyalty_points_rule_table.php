caedcd98279baecead6741d96b44094642dcb765
Denys Kozedub <kozedubdenis@gmail.com>
2022-04-22 17:54:09 +0300

    add seed

--- database/migrations/2021_11_02_135250_create_loyalty_points_rule_table.php
+++ database/migrations/2021_11_02_135250_create_loyalty_points_rule_table.php
@@ -21,37 +21,6 @@ class CreateLoyaltyPointsRuleTable extends Migration
             $table->float('accrual_value');
             $table->timestamps();
         });
-
-        $rules = [
-            [
-                'points_rule' => 'Wine +5',
-                'accrual_type' => LoyaltyPointsRule::ACCRUAL_TYPE_ABSOLUTE_POINTS_AMOUNT,
-                'accrual_value' => 5,
-            ],
-            [
-                'points_rule' => 'Coffee +1%',
-                'accrual_type' => LoyaltyPointsRule::ACCRUAL_TYPE_RELATIVE_RATE,
-                'accrual_value' => 1,
-            ],
-            [
-                'points_rule' => 'Dog Food',
-                'accrual_type' => LoyaltyPointsRule::ACCRUAL_TYPE_ABSOLUTE_POINTS_AMOUNT,
-                'accrual_value' => 10,
-            ],
-            [
-                'points_rule' => 'Vegetables +20%',
-                'accrual_type' => LoyaltyPointsRule::ACCRUAL_TYPE_RELATIVE_RATE,
-                'accrual_value' => 20,
-            ],
-        ];
-
-        foreach ($rules as $rule) {
-            $loyaltyPointsRule = new LoyaltyPointsRule();
-            $loyaltyPointsRule->points_rule = $rule['points_rule'];
-            $loyaltyPointsRule->accrual_type = $rule['accrual_type'];
-            $loyaltyPointsRule->accrual_value = $rule['accrual_value'];
-            $loyaltyPointsRule->save();
-        }
     }
 
     /**
